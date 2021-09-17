cf93a82194c23099ca8d451df5000aced841a795
amalagoli79 <89424399+amalagoli79@users.noreply.github.com>
2021-09-17 20:32:03 -0300

    Affirmation-v2
    acessa dados

---
+++ affirmations/app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -0,0 +1,9 @@
+package com.example.affirmations.adapter
+
+
+import android.content.Context
+import com.example.affirmations.data.Affirmation
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(private val context: Context, private val dataset: List<Affirmation>)  {
+}
---
+++ affirmations/app/src/main/java/com/example/affirmations/data/Affirmation.kt
@@ -0,0 +1,5 @@
+package com.example.affirmations.data
+
+class Affirmation(affirmation1: Int) {
+
+}
